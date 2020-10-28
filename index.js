da630e8b7ac457c1577d5e3de86cb1af35bd3ab8
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-10-28 18:55:45 -0500

    added form submission for local api

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email){
+    fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSRON.stringify({
+            name,
+            email
+        })
+    })
+    .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
